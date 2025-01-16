a90c51fd28febaf019507a3ba075ddaa437b62a3
Steve <steve.newstead@gmail.com>
2025-01-16 16:10:41 +0000

    tidy up sql

--- Spotify_Strava_queries.sql
+++ Spotify_Strava_queries.sql
@@ -1,28 +1,27 @@
 
 --create temp tables 
- CREATE TABLE spotify AS SELECT * FROM './demo_data/my_spotify_data/*.json';
- 
- --and spit it out as a parquat FILE
- COPY (SELECT * FROM spotify) TO 'spotify.parquet' (FORMAT PARQUET);
- 
- 
- --a spotify wrapped style thing for each year I've had spotify
- select master_metadata_album_artist_name,date_part('year', ts) listen_year, master_metadata_track_name, count(master_metadata_track_name) count_tracks
- FROM 'spotify.parquet'  as spotify
- where listen_year = 2016
- and ms_played > 60000
- group by  master_metadata_track_name, master_metadata_album_artist_name, listen_year
- order by count_tracks desc;
+CREATE TABLE spotify AS SELECT * FROM './demo_data/my_spotify_data/*.json';
 
- --create temp tables
---"Jul 18, 2017, 6:39:45 AM" 
- CREATE TABLE strava AS SELECT * FROM read_csv('./demo_data/strava/activities.csv', header = TRUE, TIMESTAMPFORMAT ='%b %-d, %Y, %I:%M:%S %p', types = {'Activity Date': 'TIMESTAMP'});
+--and spit it out as a parquat file
+COPY (SELECT * FROM spotify) TO 'spotify.parquet' (FORMAT PARQUET);
 
- 
- SELECT "Activity Date", "Elapsed Time" FROM strava;
- 
- --add a finish timestamp
- ALTER TABLE strava ADD COLUMN "Activity Date Finish" TIMESTAMP;
+
+--a spotify wrapped style thing for each year I've had spotify https://duckdb.org/docs/sql/functions/dateformat.html#format-specifiers
+SELECT master_metadata_album_artist_name,date_part('year', ts) listen_year, master_metadata_track_name, count(master_metadata_track_name) count_tracks
+FROM 'spotify.parquet'  as spotify
+WHERE listen_year = 2016
+AND ms_played > 60000
+GROUP BY master_metadata_track_name, master_metadata_album_artist_name, listen_year
+ORDER BY count_tracks desc;
+
+--Strava gives dates like this - "Jul 18, 2017, 6:39:45 AM", time stamp formating needed to import this https://duckdb.org/docs/sql/functions/dateformat.html#format-specifiers
+CREATE TABLE strava AS SELECT * FROM read_csv('./demo_data/strava/activities.csv', header = TRUE, TIMESTAMPFORMAT ='%b %-d, %Y, %I:%M:%S %p', types = {'Activity Date': 'TIMESTAMP'});
+
+
+SELECT "Activity Date", "Elapsed Time" FROM strava;
+
+--add a finish timestamp
+ALTER TABLE strava ADD COLUMN "Activity Date Finish" TIMESTAMP;
  
  --update the finish date and time
 UPDATE strava
@@ -32,7 +31,6 @@ SELECT "Activity ID", "Activity Name",Distance,"Activity Date", "Activity Date F
 FROM strava
 ORDER BY 4 DESC;
 
-
 -----------------------------------
 
 --see what I was listening to on each run
@@ -41,7 +39,7 @@ FROM spotify
 JOIN strava
    ON spotify.ts BETWEEN strava."Activity Date" AND strava."Activity Date Finish"
 WHERE ms_played > 60000 
-order by ts;			 
+ORDER BY ts;			 
 
 
  
